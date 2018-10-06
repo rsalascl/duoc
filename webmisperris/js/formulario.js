a924fb99d7efc9863166de8c8b0d78a1910fa39d
ro.salass <ro.salass@alumnos.duoc.cl>
2018-10-06 12:52:53 -0300

    nuevo archivo:  .vscode/launch.json 	nuevo archivo:  webmisperris/css/materialize.css

---
+++ webmisperris/js/formulario.js
@@ -0,0 +1,190 @@
+function showModal()
+{
+    var instance = M.Modal.getInstance(elem);
+
+}
+
+function validarDatos()
+{
+    var rutOK = Rut($("#txtRut").val());
+    var emailOK = validarEmail($("#txtEmail").val());
+    if (rutOK)
+    {
+        if (emailOK == false)
+            alert('Email no válido');
+    }
+
+}
+
+var nav4 = window.Event ? true : false;
+function soloNumeros(evt){	
+// NOTE: Backspace = 8, Enter = 13, '0' = 48, '9' = 57	
+var key = nav4 ? evt.which : evt.keyCode;	
+return (key <= 13 || (key >= 48 && key <= 57));
+}
+
+function soloLetras(e) {
+    key = e.keyCode || e.which;
+    tecla = String.fromCharCode(key).toLowerCase();
+    letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
+    especiales = [8, 37, 39, 46];
+
+    tecla_especial = false
+    for(var i in especiales) {
+        if(key == especiales[i]) {
+            tecla_especial = true;
+            break;
+        }
+    }
+
+    if(letras.indexOf(tecla) == -1 && !tecla_especial)
+        return false;
+}
+
+
+// Validador de Email
+function validarEmail(texto){
+
+    var regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    
+    if (!regex.test(texto)) {
+        return false;
+    } else {
+        return true;
+    }
+  
+  }
+
+// Validador de Rut
+
+function revisarDigito( dvr )
+{	
+	dv = dvr + ""	
+	if ( dv != '0' && dv != '1' && dv != '2' && dv != '3' && dv != '4' && dv != '5' && dv != '6' && dv != '7' && dv != '8' && dv != '9' && dv != 'k'  && dv != 'K')	
+	{		
+        alert("Debe ingresar un digito verificador valido");		
+        document.getElementById("txtRut").focus();
+        document.getElementById("txtRut").select();
+		return false;	
+	}	
+	return true;
+}
+
+function revisarDigito2( crut )
+{	
+	largo = crut.length;	
+	if ( largo < 2 )	
+	{		
+		alert("Debe ingresar el rut completo")		
+		document.getElementById("txtRut").focus();
+        document.getElementById("txtRut").select();
+		return false;	
+	}	
+	if ( largo > 2 )		
+		rut = crut.substring(0, largo - 1);	
+	else		
+		rut = crut.charAt(0);	
+	dv = crut.charAt(largo-1);	
+	revisarDigito( dv );	
+
+	if ( rut == null || dv == null )
+		return 0	
+
+	var dvr = '0'	
+	suma = 0	
+	mul  = 2	
+
+	for (i= rut.length -1 ; i >= 0; i--)	
+	{	
+		suma = suma + rut.charAt(i) * mul		
+		if (mul == 7)			
+			mul = 2		
+		else    			
+			mul++	
+	}	
+	res = suma % 11	
+	if (res==1)		
+		dvr = 'k'	
+	else if (res==0)		
+		dvr = '0'	
+	else	
+	{		
+		dvi = 11-res		
+		dvr = dvi + ""	
+	}
+	if ( dvr != dv.toLowerCase() )	
+	{		
+		alert("Rut ingresado no es válido")		
+		document.getElementById("txtRut").focus();
+        document.getElementById("txtRut").select();
+		return false	
+	}
+
+	return true
+}
+
+function Rut(texto)
+{	
+	var tmpstr = "";	
+	for ( i=0; i < texto.length ; i++ )		
+		if ( texto.charAt(i) != ' ' && texto.charAt(i) != '.' && texto.charAt(i) != '-' )
+			tmpstr = tmpstr + texto.charAt(i);	
+	texto = tmpstr;	
+	largo = texto.length;	
+
+	if ( largo < 2 )	
+	{		
+		alert("Debe ingresar el rut completo")		
+		document.getElementById("txtRut").focus();
+        document.getElementById("txtRut").select();
+		return false;	
+	}	
+
+	for (i=0; i < largo ; i++ )	
+	{			
+		if ( texto.charAt(i) !="0" && texto.charAt(i) != "1" && texto.charAt(i) !="2" && texto.charAt(i) != "3" && texto.charAt(i) != "4" && texto.charAt(i) !="5" && texto.charAt(i) != "6" && texto.charAt(i) != "7" && texto.charAt(i) !="8" && texto.charAt(i) != "9" && texto.charAt(i) !="k" && texto.charAt(i) != "K" )
+ 		{			
+			alert("El valor ingresado no corresponde a un R.U.T valido");			
+			document.getElementById("txtRut").focus();
+            document.getElementById("txtRut").select();
+			return false;		
+		}	
+	}	
+
+	var invertido = "";	
+	for ( i=(largo-1),j=0; i>=0; i--,j++ )		
+		invertido = invertido + texto.charAt(i);	
+	var dtexto = "";	
+	dtexto = dtexto + invertido.charAt(0);	
+	dtexto = dtexto + '-';	
+	cnt = 0;	
+
+	for ( i=1,j=2; i<largo; i++,j++ )	
+	{		
+		//alert("i=[" + i + "] j=[" + j +"]" );		
+		if ( cnt == 3 )		
+		{			
+			dtexto = dtexto + '.';			
+			j++;			
+			dtexto = dtexto + invertido.charAt(i);			
+			cnt = 1;		
+		}		
+		else		
+		{				
+			dtexto = dtexto + invertido.charAt(i);			
+			cnt++;		
+		}	
+	}	
+
+	invertido = "";	
+	for ( i=(dtexto.length-1),j=0; i>=0; i--,j++ )		
+		invertido = invertido + dtexto.charAt(i);	
+
+    $("#txtRut").val(invertido.toUpperCase());
+
+	if ( revisarDigito2(texto) )		
+		return true;	
+
+	return false;
+}
+
